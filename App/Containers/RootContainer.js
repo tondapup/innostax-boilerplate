32712a762d14b8bfeb45c402f1315cf8fb03c8b7
Nidhi Deswal <nidhi.deswal@innostax.com>
2018-08-02 17:34:55 +0530

    integrated onesignal for android.

--- App/Containers/RootContainer.js
+++ App/Containers/RootContainer.js
@@ -7,16 +7,42 @@ import ReduxPersist from '../Config/ReduxPersist'
 
 // Styles
 import styles from './Styles/RootContainerStyles'
-
+import OneSignal from 'react-native-onesignal';
+import * as commonConstants from '../Constants'
 class RootContainer extends Component {
-  componentDidMount () {
+
+  componentWillMount() {
+    OneSignal.init(commonConstants.ONE_SIGNAL_APP_ID);
+    OneSignal.inFocusDisplaying(2);
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+    OneSignal.configure();
+  }
+
+  componentDidMount() {
     // if redux persist is not active fire startup action
     if (!ReduxPersist.active) {
       this.props.startup()
     }
   }
+  onReceived = (notification) => {
+    console.log("Notification received: ", notification);
+  }
 
-  render () {
+  onOpened = (openResult) => {
+    console.log('Opened')
+  }
+  onIds = (device) => {
+    console.log('Device info: ', device);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+  render() {
     return (
       <View style={styles.applicationView}>
         <StatusBar barStyle='light-content' />
